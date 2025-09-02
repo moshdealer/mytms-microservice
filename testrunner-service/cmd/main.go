3fc4dda333736089e52ba4c0bd0659ef1eaf372c
moshdealer <mikhailkonchakov@gmail.com>
2025-09-02 19:40:35 +0300

    Долил докерфайлы

--- testrunner-service/cmd/main.go
+++ testrunner-service/cmd/main.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "testrunner-service running",
+		})
+	})
+
+	r.Run(":8083")
+}
